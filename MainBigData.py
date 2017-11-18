1c1faaa16a73318e59cea7eac8d7ad1acc38b8bf
Alfred951 <lutisluis@gmail.com>
2017-11-18 18:49:51 -0500

    Adding file reading and term reducing

--- MainBigData.py
+++ MainBigData.py
@@ -1,10 +1,19 @@
 import re
 from pyspark import SparkContext, SparkConf
+from pyspark.mllib.feature import HashingTF
+from pyspark.mllib.feature import IDF
 
 if __name__ == "__main__":
     sc = SparkContext(appName="DocumentClustering")
-    files_RDD = sc.wholeTextFiles("/user/jalvar53/datasets/gutenberg")
+    files_RDD = sc.wholeTextFiles("hdfs:///user/jalvar53/datasets/gutenberg/*")
     documents_names = files_RDD.keys()
-    documents = documents_names.values().map(lambda document: re.split('\W+', document))
-    print documents_names
-    print documents
+    documents = files_RDD.values().map(lambda doc: re.split('[^a-zA-Z]+',doc))
+
+    #From https://spark.apache.org/docs/2.1.1/mllib-feature-extraction.html#tf-idf
+    hashingTF = HashingTF()
+    tf = hashingTF.transform(documents)
+    tf.cache()
+    idf = IDF().fit(tf)
+    tfidf = idf.transform(tf)
+
+    print(tfidf.take(2))
